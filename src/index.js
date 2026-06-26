cd3e7c5e90c1e93a9764adcc3eb1197d7674941a
Damil Adeniyi <damiladeniyi301@gmail.com>
2026-06-26 13:10:35 -0500

    create part of addTask section

--- src/index.js
+++ src/index.js
@@ -1,2 +1,63 @@
 import "./styles.css";
 
+const togglebtn = document.querySelector(".toggleBtn")
+const sidebar = document.querySelector(".sidebar")
+const addTaskBtn = document.querySelector("#submit-btn")
+const taskCont = document.querySelector("#tasks-cont")
+const taskTitle = document.querySelector("#task-title")
+const taskDesc = document.querySelector("#task-desc")
+const dueDate = document.querySelector("#due-date")
+const priority = document.querySelector("#priority")
+
+
+
+
+addTaskBtn.addEventListener('click', function(e) {
+
+    e.preventDefault()
+
+    // task cont
+    let tasks = document.createElement("div")
+    tasks.innerHTML = `<div><div class="col"><h2 class="newTaskTitle"></h2><p class="newTaskDesc"></p>
+    </div><div class="row"><h3 class="newTaskDueDate"></h3><h3 class="newTaskPriority"></h3></div></div>`
+
+
+    tasks.classList.add('tasks')
+
+
+    // task
+    let col = document.createElement("div")
+    col.classList.add('col')
+    let newTaskTitle = document.createElement("li")
+    newTaskTitle.classList.add('title')
+    let newTaskDesc = document.createElement("p")
+    newTaskDesc.classList.add('para')
+
+    let row = document.createElement("div")
+    row.classList.add('row')
+    let newTaskDueDate = document.createElement("h3")
+    newTaskDueDate.classList.add('due-date')
+    let newTaskPriority = document.createElement("h3")
+    newTaskPriority.classList.add('Priority')
+
+
+    newTaskTitle.textContent = taskTitle.value
+    newTaskDesc.textContent = taskDesc.value
+    newTaskDueDate.textContent = dueDate.value
+    newTaskPriority.textContent = priority.value
+
+   
+    col.appendChild(newTaskTitle)
+    col.appendChild(newTaskDesc)
+
+
+    tasks.appendChild(col)
+
+    row.appendChild(newTaskDueDate)
+    row.appendChild(newTaskPriority)
+
+
+    tasks.appendChild(row)
+
+    taskCont.appendChild(tasks)
+})
