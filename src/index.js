52a93b5934968783b923e5129e2ad6d054ec072d
Damil Adeniyi <damiladeniyi301@gmail.com>
2026-06-30 11:35:46 -0500

    add styles dashboard add add task event to index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,43 @@
 import "./styles.css";
+import { format, compareAsc } from "date-fns";
+
+class Project {
+    static numProjects = 0;
+
+    constructor(name='default') {
+        this.name = name;
+        this.status = 'pending';
+        this.tasks = {};
+        Project.numProjects++
+    }
+
+    addTask(task) {
+      this.tasks[task.title] = Task;
+    }
+}
+
+class Task {
+    constructor(title = 'Get a task done', 
+                desc = 'Watch Netflix', 
+                dueDate = new Date().toLocaleDateString(),
+                priority = 'low') {
+            this.title = title;
+            this.desc = desc;
+            this.dueDate = dueDate;
+            this.priority = priority;
+
+    }
+}
+
+const project = new Project();
+project.name = 'Get Working'
+const task = new Task();
+const task1 = new Task('Read TOD', 'Continue classes', format(new Date(2026, 6, 30), 'LLL d'), 'low');
+const task2 = new Task('Read Freecodecamp', 'Continue OOP', format('07/02/2026', 'LLL d'), 'high');
+
+project.addTask(task1)
+project.addTask(task2)
+// console.log(project);
 
 const togglebtn = document.querySelector(".toggleBtn")
 const sidebar = document.querySelector(".sidebar")
@@ -10,54 +49,32 @@ const dueDate = document.querySelector("#due-date")
 const priority = document.querySelector("#priority")
 
 
-
-
-addTaskBtn.addEventListener('click', function(e) {
-
+addTaskBtn.addEventListener('click', function (e) {
     e.preventDefault()
 
     // task cont
     let tasks = document.createElement("div")
-    tasks.innerHTML = `<div><div class="col"><h2 class="newTaskTitle"></h2><p class="newTaskDesc"></p>
-    </div><div class="row"><h3 class="newTaskDueDate"></h3><h3 class="newTaskPriority"></h3></div></div>`
+    tasks.innerHTML = `<div>
+                            <div class="col">
+                                <div class="featCont">
+                                     <div class="titlecont">
+                                    <button class="checkTask">✔</button>
+                                    <h2 class="newTaskTitle">${taskTitle.value}</h2>
+                                </div>
 
+                                <div class="propCont">
+                                    <h3 class="newTaskDueDate">${format((dueDate.value), 'LLL d')}</h3>
+                                    <h3 class="newTaskPriority">${priority.value}</h3>
+
+                                </div>
+
+                                </div> 
+                                
+                                <p class="newTaskDesc">${taskDesc.value}</p>
+                            </div>
+                            </div>`
 
     tasks.classList.add('tasks')
-
-
-    // task
-    let col = document.createElement("div")
-    col.classList.add('col')
-    let newTaskTitle = document.createElement("li")
-    newTaskTitle.classList.add('title')
-    let newTaskDesc = document.createElement("p")
-    newTaskDesc.classList.add('para')
-
-    let row = document.createElement("div")
-    row.classList.add('row')
-    let newTaskDueDate = document.createElement("h3")
-    newTaskDueDate.classList.add('due-date')
-    let newTaskPriority = document.createElement("h3")
-    newTaskPriority.classList.add('Priority')
-
-
-    newTaskTitle.textContent = taskTitle.value
-    newTaskDesc.textContent = taskDesc.value
-    newTaskDueDate.textContent = dueDate.value
-    newTaskPriority.textContent = priority.value
-
-   
-    col.appendChild(newTaskTitle)
-    col.appendChild(newTaskDesc)
-
-
-    tasks.appendChild(col)
-
-    row.appendChild(newTaskDueDate)
-    row.appendChild(newTaskPriority)
-
-
-    tasks.appendChild(row)
-
     taskCont.appendChild(tasks)
-})
+
+});
